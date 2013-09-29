a0f0a57256b8c7114d7076822bbbb2785573266a
ssanchez <dev@spencersanchez.com>
2013-09-29 12:48:22 -0400

    plus one button back

--- js/libs.js
+++ js/libs.js
@@ -1,1 +1,14 @@
-
+/*!
+* Google +1 button
+*/
+window.___gcfg = {
+	lang: 'en-US'
+};
+(function() {
+	var s, po = document.createElement('script');
+	po.type = 'text/javascript';
+	po.async = true;
+	po.src = 'https://apis.google.com/js/plusone.js';
+	s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(po, s);
+})();
