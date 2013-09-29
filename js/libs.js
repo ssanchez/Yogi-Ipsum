25b73209573cd2b78d05872332471402ed13ec31
ssanchez <dev@spencersanchez.com>
2013-09-28 23:43:07 -0400

    superflous social media code

--- js/libs.js
+++ js/libs.js
@@ -1,19 +1,1 @@
-/*!
-* Twitter widget
-*/
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
-/*!
-* Google +1 button
-*/
-window.___gcfg = {
-	lang: 'en-US'
-};
-(function() {
-	var s, po = document.createElement('script');
-	po.type = 'text/javascript';
-	po.async = true;
-	po.src = 'https://apis.google.com/js/plusone.js';
-	s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(po, s);
-})();
